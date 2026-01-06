a9b4d2c3d4323f6294cab8e5376eb624c2438168
Md. Amanullah Rijve <sheikhrijve885@gmail.com>
2026-01-06 14:32:30 +0600

    Add files via upload

---
+++ Arithmatic.java
@@ -0,0 +1,12 @@
+public class Arithmatic{
+    public static void main(String[] args){
+        int x = 7;
+        int y = 9;
+        
+        System.out.println("+ Operator "+ (x+y));
+        System.out.println("- Operator "+( x-y));
+        System.out.println("* Operator "+( x*y));
+        System.out.println("/ Operator "+ (x/y));
+        System.out.println("% Operator "+ (x%y));
+    }
+}
