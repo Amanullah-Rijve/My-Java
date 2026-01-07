f991fc61e8096ef73b8a96c7a2c41e8d114d0b65
Md. Amanullah Rijve <sheikhrijve885@gmail.com>
2026-01-07 23:00:53 +0600

    Add files via upload

---
+++ Two.java
@@ -0,0 +1,8 @@
+public class Two{
+    public static void main(String[] args){
+        String name = "Khalifa ";
+        System.out.println("Hello " + name +"you love " + 69 +" ?");
+        System.out.println("F U " + name);
+        System.out.println("Bitch " + name);
+    }
+}
