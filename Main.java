5a43adc8d30fa973c0a02304a773e4dcf91004d9
Md. Amanullah Rijve <sheikhrijve885@gmail.com>
2026-01-08 00:01:05 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args){
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        System.out.println("The Length: " + alphabet.length());
+    }
+}
