3c69b8797666bd5ba44d2060f207a6d40c8f0d62
Md. Amanullah Rijve <sheikhrijve885@gmail.com>
2026-01-19 20:42:41 +0600

    Add files via upload

---
+++ TypeCasting.java
@@ -0,0 +1,19 @@
+// public class TypeCasting{
+//     public static void main(String[] args){
+//         int num = 2;
+//         double Dnum = num;
+
+//         System.out.println(num);
+//         System.out.println(Dnum);
+//     }
+// }
+
+// public class TypeCasting{
+//     public static void main(String[] args){
+//         float num = 2.34f;
+//         int Inum = (int)num;
+
+//         System.out.println(num);
+//         System.out.println(Inum);
+//     }
+// }
