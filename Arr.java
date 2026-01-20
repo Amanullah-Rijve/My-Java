8491e78783d9697e4f93ef76d758cc671c4d8b48
Md. Amanullah Rijve <sheikhrijve885@gmail.com>
2026-01-21 00:02:52 +0600

    Add files via upload

---
+++ Arr.java
@@ -0,0 +1,24 @@
+public class Arr{
+    public static void main(String[] args){
+        String[] carName ={"farari","tesla","bmw","honda","tata","suzuki"};
+        //System.out.println(carName[2]);
+        // System.out.println(carName.length);
+        // for(int i =0;i<carName.length;i++){
+        //     System.out.println(carName[i]);
+        //}
+        // ! calculate sum
+        int[] num ={10,20,30,40,50,66,69};
+        // int sum = 0;
+
+        // for(int i=0; i<num.length;i++){
+        //     sum+=num[i];
+        // }
+        // System.out.println(sum);
+        // for(String cars : carName){
+        //     System.out.println(cars);
+        // }
+        // for(int nums: num){
+        //     System.out.println(nums);
+        // }
+    }
+}
